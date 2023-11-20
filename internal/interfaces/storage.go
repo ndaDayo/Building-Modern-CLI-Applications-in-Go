46dae742b48c4a1e97ddd0d1c93b545eb21f1806
ndaDayo <hayato711+nda@gmail.com>
2023-11-20 10:55:17 +0900

    add: storage interfaces

---
+++ internal/interfaces/storage.go
@@ -0,0 +1,9 @@
+package interfaces
+
+type Storage interface {
+	Upload(bytes []byte, filename string) (string, string, error)
+	SaveMetadata(audio *models.Audio) error
+	List() ([]*models.Audio, error)
+	GetByID(id string) (*models.Audio, error)
+	Delete(id string, tag string) error
+}
