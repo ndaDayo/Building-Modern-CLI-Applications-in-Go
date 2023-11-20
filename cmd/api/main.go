223a04bde2bfedcdcde1da09764435f8d77d65de
ndaDayo <hayato711+nda@gmail.com>
2023-11-20 10:26:20 +0900

    add: api pkg

---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "p", 8000, "Port for metadata service")
+	flag.Parse()
+	fmt.Printf("Starting API at http://localhost:%d\n", port)
+	metadataService.Run(port)
+}
