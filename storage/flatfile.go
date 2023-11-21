eaa58100b627ce92ea487eaec1aed17a174e5cf9
ndaDayo <hayato711+nda@gmail.com>
2023-11-21 22:07:37 +0900

    wip: storage flatfile

--- storage/flatfile.go
+++ storage/flatfile.go
@@ -1,5 +1,27 @@
 package storage
 
+import "github.com/ndaDayo/Audio-Metadata-CLI/models"
+
 type FlatFile struct {
 	Name string
+}
+
+func (f FlatFile) GetByID(id string) (*models.Audio, error) {
+	return nil
+}
+
+func (f FlatFile) SaveMetadata(audio *models.Audio) error {
+	return nil
+}
+
+func (f FlatFile) Upload(bytes []byte, filename string) (string, string, error) {
+	return nil
+}
+
+func (f FlatFile) List() ([]*models.Audio, error) {
+	return nil
+}
+
+func (f FlatFile) Delete(id string) error {
+	return nil
 }
