65fd3a03eb0d63ea210d8674769437767bfa4e5a
ndaDayo <hayato711+nda@gmail.com>
2023-11-24 11:11:00 +0900

    add: getbyid

--- storage/flatfile.go
+++ storage/flatfile.go
@@ -1,13 +1,37 @@
 package storage
 
-import "github.com/ndaDayo/Audio-Metadata-CLI/models"
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/ndaDayo/Audio-Metadata-CLI/models"
+)
 
 type FlatFile struct {
 	Name string
 }
 
 func (f FlatFile) GetByID(id string) (*models.Audio, error) {
-	return nil
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	metadataFilePath := filepath.Join(dirname, "audiofile", id, "metadata.json")
+	if _, err := os.Stat(metadataFilePath); errors.Is(err, os.ErrNotExist) {
+		_ = os.Mkdir(metadataFilePath, os.ModePerm)
+	}
+
+	file, err := os.ReadFile(metadataFilePath)
+	if err != nil {
+		return nil, err
+	}
+	data := models.Audio{}
+
+	err = json.Unmarshal([]byte(file), &data)
+	return &data, err
 }
 
 func (f FlatFile) SaveMetadata(audio *models.Audio) error {
