ad793f14c15bbb490302a311693fae304d6dbf3b
ndaDayo <hayato711+nda@gmail.com>
2023-11-29 22:38:50 +0900

    wip: add test

--- ch04/calculator/cmd/add.go
+++ ch04/calculator/cmd/add.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"calculator/storage"
+	"fmt"
+	"strconv"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,6 +13,23 @@ var addCmd = &cobra.Command{
 	Use:   "add number",
 	Short: "add value",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 1 {
+			fmt.Println("only accepts a single argument")
+			return
+		}
+		if len(args) == 0 {
+			fmt.Println("command requires input value")
+			return
+		}
+		floatVal, err := strconv.ParseFloat(args[0], 64)
+		if err != nil {
+			fmt.Printf("unable to parse input[%s]: %v",
+				args[0], err)
+			return
+		}
+		value := storage.GetValue()
+		value += floatVal
+		storage.SetValue(value)
 	},
 }
 
--- ch04/calculator/cmd/add_test.go
+++ ch04/calculator/cmd/add_test.go
@@ -1,39 +1,46 @@
 package cmd
 
 import (
+	"bytes"
+	"calculator/storage"
+	"strings"
 	"testing"
 )
 
 func TestAddCmdRun(t *testing.T) {
+	initialValue := 0.0
+	storage.SetValue(initialValue)
+
 	tests := []struct {
 		name    string
 		args    []string
 		wantOut string
-		wantErr bool
 	}{
 		{
 			name:    "No Arguments",
 			args:    []string{},
 			wantOut: "command requires input value\n",
-			wantErr: true,
 		},
 		{
 			name:    "Multiple Arguments",
 			args:    []string{"10", "20"},
 			wantOut: "only accepts a single argument\n",
-			wantErr: true,
 		},
-		{
-			name:    "Invalid Argument",
-			args:    []string{"abc"},
-			wantOut: "unable to parse input[abc]",
-			wantErr: true,
-		},
-		{
-			name:    "Valid Argument",
-			args:    []string{"10"},
-			wantOut: "",
-			wantErr: false,
-		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := addCmd
+			b := bytes.NewBufferString("")
+			cmd.SetOut(b)
+			cmd.SetErr(b)
+			cmd.SetArgs(tt.args)
+
+			cmd.Execute()
+
+			if !strings.Contains(b.String(), tt.wantOut) {
+				t.Errorf("TestAddCmd %s: got output = %v, want %v", tt.name, b.String(), tt.wantOut)
+			}
+		})
 	}
 }
