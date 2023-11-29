5ca178ca8a8c3402edc4ed2a410c65e99b15f6ac
ndaDayo <hayato711+nda@gmail.com>
2023-11-29 21:32:26 +0900

    add: test case

--- ch04/calculator/cmd/add_test.go
+++ ch04/calculator/cmd/add_test.go
@@ -1,16 +1,39 @@
 package cmd
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestAddCmd(t *testing.T) {
+func TestAddCmdRun(t *testing.T) {
 	tests := []struct {
-		name      string
-		args      []string
-		wantValue float64
-		wantErr   bool
+		name    string
+		args    []string
+		wantOut string
+		wantErr bool
 	}{
-		{"No Argument", []string{}, 0, true},
+		{
+			name:    "No Arguments",
+			args:    []string{},
+			wantOut: "command requires input value\n",
+			wantErr: true,
+		},
+		{
+			name:    "Multiple Arguments",
+			args:    []string{"10", "20"},
+			wantOut: "only accepts a single argument\n",
+			wantErr: true,
+		},
+		{
+			name:    "Invalid Argument",
+			args:    []string{"abc"},
+			wantOut: "unable to parse input[abc]",
+			wantErr: true,
+		},
+		{
+			name:    "Valid Argument",
+			args:    []string{"10"},
+			wantOut: "",
+			wantErr: false,
+		},
 	}
-
-    for _, tt := range tests
 }
