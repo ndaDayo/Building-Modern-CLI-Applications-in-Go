0c4d05f53b4ef5834fac17997246822f407b566a
ndaDayo <hayato711+nda@gmail.com>
2023-12-02 14:17:04 +0900

    add: storage files

--- ch04/calculator/cmd/add_test.go
+++ ch04/calculator/cmd/add_test.go
@@ -2,45 +2,29 @@ package cmd
 
 import (
 	"bytes"
-	"calculator/storage"
-	"strings"
+	"os"
 	"testing"
 )
 
-func TestAddCmdRun(t *testing.T) {
-	initialValue := 0.0
-	storage.SetValue(initialValue)
+func setArgs(args []string) {
+	os.Args = append([]string{os.Args[0]}, args...)
+}
 
-	tests := []struct {
-		name    string
-		args    []string
-		wantOut string
-	}{
-		{
-			name:    "No Arguments",
-			args:    []string{},
-			wantOut: "command requires input value\n",
-		},
-		{
-			name:    "Multiple Arguments",
-			args:    []string{"10", "20"},
-			wantOut: "only accepts a single argument\n",
-		},
+func TestAddCmdRun(t *testing.T) {
+	cmd := addCmd
+
+	var stdout bytes.Buffer
+	cmd.SetOut(&stdout)
+
+	cmd.SetArgs([]string{})
+
+	err := cmd.Execute()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			cmd := addCmd
-			b := bytes.NewBufferString("")
-			cmd.SetOut(b)
-			cmd.SetErr(b)
-			cmd.SetArgs(tt.args)
-
-			cmd.Execute()
-
-			if !strings.Contains(b.String(), tt.wantOut) {
-				t.Errorf("TestAddCmd %s: got output = %v, want %v", tt.name, b.String(), tt.wantOut)
-			}
-		})
+	expectedOutput := "Hello, John!\n"
+	if stdout.String() != expectedOutput {
+		t.Errorf("Expected output: %q, but got: %q", expectedOutput, stdout.String())
 	}
 }
--- ch04/calculator/main.go
+++ ch04/calculator/main.go
@@ -1,11 +1,22 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "calculator/cmd"
+import (
+	"calculator/cmd"
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("error reading in config: ", err)
+	}
 	cmd.Execute()
 }
