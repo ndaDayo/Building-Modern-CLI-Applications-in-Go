8b361dba19485a5696585ff76479c7f86c2e382c
ndaDayo <hayato711+nda@gmail.com>
2023-12-02 14:22:24 +0900

    add: command, subcommand

---
+++ ch05/cmd/command.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// commandCmd represents the command command
+var commandCmd = &cobra.Command{
+	Use: "command",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("command called")
+		localFlag, _ := cmd.Flags().GetString("localFlag")
+		if localFlag != "" {
+			fmt.Printf("localFlag is set to %s\n", localFlag)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(commandCmd)
+}
